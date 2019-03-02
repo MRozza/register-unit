0f73b7fca210078c52812f521557c94d737dc435
MRozza <Mohamed.a.rozza@gmail.com>
2019-03-03 12:35:08 +1300

    first commit

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+exports.register = function(req, res) {
+  message = '';
+  if (req.method == 'POST') {
+    var post = req.body;
+    var userName = post.user_name;
+    var password = post.password;
+    var name = post.name;
+    console.log(userName);
+    console.log(password);
+    var sql =
+      "INSERT INTO `users`(`name`,`user_name`, `password`) VALUES ('" +
+      name +
+      "','" +
+      userName +
+      "','" +
+      password +
+      "')";
+
+    db.query(sql, function(err, result) {
+      message = 'Succesfully! Your account has been created.';
+
+      sql =
+        "SELECT id, name, user_name FROM `users` WHERE `user_name`='" +
+        userName +
+        "' and password = '" +
+        password +
+        "'";
+      db.query(sql, function(err, results) {
+        console.log(results);
+
+        if (results && results.length) {
+          res.redirect('http://localhost:8091/login');
+        } else {
+          message = 'Invalid Username or Password!';
+          res.render('index.ejs', { message: message });
+        }
+      });
+    });
+  } else {
+    res.render('signup');
+  }
+};
