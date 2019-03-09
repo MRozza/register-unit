f9197c17487e12acc71856c66430e4b8738de90a
MRozza <Mohamed.a.rozza@gmail.com>
2019-03-09 22:48:23 +1300

    trigger build

--- travis-ci-trigger.js
+++ travis-ci-trigger.js
@@ -1,6 +1,6 @@
 var Travis = require('travis-ci');
 
-var repo = 'https://github.com/MRozza/login-unit';
+var repo = 'https://github.com/MRozza/login-unit.git';
 
 var travis = new Travis({
   version: '2.0.0'
